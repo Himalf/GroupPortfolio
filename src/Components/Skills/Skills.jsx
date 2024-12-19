69aa561e232bc38ea33cb4b12c9cd6818c53160c
nimesh631 <nimeshkayastha067@gmail.com>
2024-12-19 17:11:18 +0545

    fix: change background color and margin of code in Skills section

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Skills() {
   return (
     <div>
-      <section id="Skills" className='bg-black bg-opacity-30 shadow-lg rounded-2xl p-5 text-orange-500 '>
+      <section id="Skills" className='bg-blue-950 shadow-lg rounded-2xl p-5 text-white '>
         <h2 className='text-3xl font-semibold mb-10'>Skills</h2>
           
           <div className='grid lg:grid-cols-4 gap-3 md:grid-cols-2'>
@@ -11,13 +11,13 @@ function Skills() {
             <h3 className='text-xl mb-2 flex items-center'>Frontend Development</h3>
             <ul>
               <li className='flex items-center py-1'>
-                <span className='bg-yellow-600 rounded-md'>⚡</span> JavaScript
+                <span className='bg-yellow-600 rounded-md mr-2'>⚡</span> JavaScript
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-blue-500 rounded-md'>⚛️</span> React
+                <span className='bg-blue-500 rounded-md mr-2'>⚛️</span> React
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-green-500 rounded-md'>🌐</span> Next.js
+                <span className='bg-green-500 rounded-md mr-2'>🌐</span> Next.js
               </li>
             </ul>
           </div>
@@ -27,13 +27,13 @@ function Skills() {
             <h3 className='text-xl mb-2 flex items-center'>Backend and Database</h3>
             <ul>
               <li className='flex items- py-1'>
-                <span className='bg-green-500 rounded-md'>🛠️</span> Node.js
+                <span className='bg-green-500 rounded-md mr-2'>🛠️</span> Node.js
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-red-500 rounded-md'>🚀</span> Express.js
+                <span className='bg-red-500 rounded-md mr-2'>🚀</span> Express.js
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-gray-500 rounded-md'>✨</span> Mysql
+                <span className='bg-gray-500 rounded-md mr-2'>✨</span> Mysql
               </li>
             </ul>
           </div>
@@ -43,14 +43,14 @@ function Skills() {
             <h3 className='text-xl mb-2 flex items-center'>Essential Skills</h3>
             <ul>
               <li className='flex items-center py-1'>
-                <span className='bg-black rounded-md'>🔧</span> Git
+                <span className='bg-black rounded-md mr-2'>🔧</span> Git
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-blue-500 rounded-md'>🎨</span> Canva
+                <span className='bg-blue-500 rounded-md mr-2'>🎨</span> Canva
               </li>
 
               <li className='flex items-center py-1'>
-                <span className='bg-green-500 rounded-md'>🎨</span> Excel
+                <span className='bg-green-500 rounded-md mr-2'>🎨</span> Excel
               </li>
             </ul>
           </div>
@@ -59,14 +59,14 @@ function Skills() {
             <h3 className='text-xl mb-2 flex items-center'>Soft Skills</h3>
             <ul>
               <li className='flex items-center py-1'>
-                <span className='bg-blue-800 rounded-md'>✔️</span> Team Collaboration
+                <span className='bg-blue-800 rounded-md mr-2'>✔️</span> Team Collaboration
               </li>
               <li className='flex items-center py-1'>
-                <span className='bg-blue-200 rounded-md'>✔️</span> Communication
+                <span className='bg-blue-200 rounded-md mr-2'>✔️</span> Communication
               </li>
 
               <li className='flex items-center py-1'>
-                <span className='bg-blue-500 rounded-md'>✔️</span> Problem Solving
+                <span className='bg-blue-500 rounded-md mr-2'>✔️</span> Problem Solving
               </li>
             </ul>
           </div>
