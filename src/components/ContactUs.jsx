9272efed5026c48feb3c6a7ec5cc6a057c1732b5
Aayush850 <aayushpaudel87@gmail.com>
2024-12-19 16:09:29 +0545

    fix: add required validation and labels to contact us form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiPhone } from "react-icons/ci";
 import { CiLocationOn } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
@@ -20,13 +20,21 @@ const cardData = [
 ];
 
 const ContactUs = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const handlePhoneNumberChange = (e) => {
+    const value = e.target.value;
+    // Allow only digits, +, and space
+    const formattedValue = value.replace(/[^0-9+\s]/g, "");
+    setPhoneNumber(formattedValue);
+  };
+
   return (
     <section className="contact-us">
       <div className="get-in-touch">
         <h2 className="section-title">Get In Touch</h2>
         <p>
           Feel free to contact us? submit your queries here and we will get back
-          to you as soon as possible
+          to you as soon as possible.
         </p>
         <div className="card-container">
           {cardData.map((item, index) => {
@@ -44,21 +52,54 @@ const ContactUs = () => {
         <div className="form-container">
           <form className="form">
             <div className="input-group">
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="Email" />
+              <div className="input-container">
+                <label htmlFor="name" name="name">
+                  Name:
+                </label>
+                <input type="text" placeholder="Name" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="email" name="email">
+                  Email:
+                </label>
+                <input type="email" placeholder="Email" required />
+              </div>
             </div>
             <div className="input-group">
-              <input type="tel" placeholder="Number" />
-              <input type="text" placeholder="Subject" />
+              <div className="input-container">
+                <label htmlFor="number">Number:</label>
+                <input
+                  type="tel"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumberChange}
+                  placeholder="Number"
+                  name="number"
+                  pattern="[\+]?[0-9]{1,4}[ -]?[0-9]{6,15}"
+                  required
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="subject" name="subject">
+                  Subject:
+                </label>
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  name="subject"
+                  required
+                />
+              </div>
             </div>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Write Message....."
-            ></textarea>
-            <button type="submit" onClick={(e) => e.preventDefault()}>
-              Submit
-            </button>
+            <div className="input-container">
+              <label htmlFor="message">Write a message:</label>
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Write Message....."
+                required
+              ></textarea>
+            </div>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
